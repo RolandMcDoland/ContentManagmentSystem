420b9ff3aa7b8ee11c911549ce23e76b4b6fb136
Szymon Liszka <liszkaszymon@gmail.com>
2021-01-20 02:36:17 +0100

    Add creating, editing, deleting users

--- cmsapp/views.py
+++ cmsapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group
 
 from .models import Article, Comment, User
 
@@ -29,10 +30,33 @@ def user_list(request):
         temp_users = User.objects.order_by('id')
         users = []
         for u in temp_users:
-            users.append({'username': u.username, 'role': 'admin'})
+            users.append({'id': u.id,
+                          'username': u.username,
+                          'role': u.groups.first() if u.groups.count() > 0 else ''})
 
         return render(request, 'management/user_list.html', {'users': users})
     return render(request, 'bad_permission.html')
+
+
+def user_edit(request, user_id):
+    u = get_object_or_404(User, pk=user_id)
+    user = {'id': u.id, 'username': u.username, 'group': u.groups.first() if u.groups.count() > 0 else ''}
+    groups = Group.objects.all()
+    return render(request, 'management/user_edit.html', {'edit_user': user, 'groups': groups})
+
+
+def user_save(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.groups.clear()
+    gr = request.POST.get('selected_group')
+    user.groups.set([gr])
+    return redirect('cmsapp:user_list')
+
+
+def user_delete(request, user_id):
+    u = get_object_or_404(User, pk=user_id)
+    u.delete()
+    return redirect('cmsapp:user_list')
 
 def article_new(request):
     return render(request, 'management/article_new.html')
@@ -62,3 +86,31 @@ def custom_authentication(request):
         return HttpResponseRedirect('/')
     else:
         return render(request, 'account/login.html', {'error': True})
+
+
+def custom_register(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    else:
+        return render(request, 'account/register.html')
+
+
+def custom_create_account(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    else:
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        fname = request.POST.get('first_name')
+        lname = request.POST.get('last_name')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        if pass1 == pass2:
+            user = User.objects.create_user(username=username, email=email, password=pass1)
+            user.first_name = fname
+            user.last_name = lname
+            user.groups.set([3])    # id=3 -> User group
+            user.save()
+
+        return render(request, 'account/register.html')
